205f91c344014ac221c0b7862f7be57078c84006
zhangjingjing1 <zhangjingjing1@sensetime.com>
2020-10-20 17:47:45 +0800

    basic function of speech processing

--- SpeechProcessing/SpeechProcessing/Form1.Designer.cs
+++ SpeechProcessing/SpeechProcessing/Form1.Designer.cs
@@ -29,50 +29,77 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.recog_result = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.audio_format = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.synthesis_input = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.richTextBox2);
+            this.groupBox1.Controls.Add(this.recog_result);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.audio_format);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 28);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(367, 301);
+            this.groupBox1.Size = new System.Drawing.Size(365, 364);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "语音识别";
             // 
-            // comboBox1
+            // recog_result
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "PCM",
-            "WAV",
-            "AMR",
-            "M4A"});
-            this.comboBox1.Location = new System.Drawing.Point(76, 68);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 3;
+            this.recog_result.Location = new System.Drawing.Point(11, 156);
+            this.recog_result.Name = "recog_result";
+            this.recog_result.Size = new System.Drawing.Size(314, 192);
+            this.recog_result.TabIndex = 6;
+            this.recog_result.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "选择文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(11, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "D:\\BUAA\\SpeechProcessing\\samples\\16k.wav";
+            // 
+            // audio_format
+            // 
+            this.audio_format.FormattingEnabled = true;
+            this.audio_format.Items.AddRange(new object[] {
+            "pcm",
+            "wav",
+            "amr",
+            "m4a"});
+            this.audio_format.Location = new System.Drawing.Point(76, 68);
+            this.audio_format.Name = "audio_format";
+            this.audio_format.Size = new System.Drawing.Size(121, 20);
+            this.audio_format.TabIndex = 3;
             // 
             // label2
             // 
@@ -105,60 +132,35 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.synthesis_input);
             this.groupBox2.Location = new System.Drawing.Point(413, 28);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(207, 287);
+            this.groupBox2.Size = new System.Drawing.Size(222, 364);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "语音合成";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(110, 249);
+            this.button1.Location = new System.Drawing.Point(141, 335);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "开始合成";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // synthesis_input
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(6, 20);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(195, 217);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.synthesis_input.Location = new System.Drawing.Point(6, 20);
+            this.synthesis_input.Name = "synthesis_input";
+            this.synthesis_input.Size = new System.Drawing.Size(202, 295);
+            this.synthesis_input.TabIndex = 0;
+            this.synthesis_input.Text = "";
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(11, 21);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(218, 21);
-            this.textBox1.TabIndex = 4;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(250, 21);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "选择文件";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(11, 156);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(314, 131);
-            this.richTextBox2.TabIndex = 6;
-            this.richTextBox2.Text = "";
             // 
             // Form1
             // 
@@ -180,14 +182,14 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox audio_format;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox synthesis_input;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox recog_result;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
--- SpeechProcessing/SpeechProcessing/Form1.cs
+++ SpeechProcessing/SpeechProcessing/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace SpeechProcessing
 {
@@ -27,9 +30,56 @@ namespace SpeechProcessing
             }
         }
 
+        // 调用getAccessToken()获取的 access_token建议根据expires_in 时间 设置缓存
+        // 百度云中开通对应服务应用的 API Key 建议开通应用的时候多选服务
+        private static String API_KEY = "ZiUC7H5c7Fkdan8n6Xj5vG3t";
+        // 百度云中开通对应服务应用的 Secret Key
+        private static String SECRET_KEY = "ZAV5r9h6BhmIeSa5zYx2L4plskh7IcsD";
+        
         private void button2_Click(object sender, EventArgs e)
         {
             //开始识别按钮
+            //请求语音识别接口
+            var data = File.ReadAllBytes(textBox1.Text);
+            var options = new Dictionary<string, object>{
+                {"dev_pid", 1537}
+            };
+            var client = new Baidu.Aip.Speech.Asr(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;
+            Console.Write(this.audio_format.Text);
+            JObject result = new JObject();
+            switch(this.audio_format.Text){
+                case "pcm":
+                    result = client.Recognize(data, "pcm", 16000, options);
+                    break;
+                case "wav":
+                    result = client.Recognize(data, "wav", 16000, options);
+                    break;
+                case "amr":
+                    result = client.Recognize(data, "amr", 16000, options);
+                    break;
+                case "m4a":
+                    result = client.Recognize(data, "m4a", 16000, options);
+                    break;
+            }
+            this.recog_result.Text = result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new Baidu.Aip.Speech.Tts(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+            var option = new Dictionary<string, object>()
+            {
+                {"spd", 5}, // 语速
+                {"vol", 7}, // 音量
+                {"per", 4}  // 发音人，4：情感度丫丫童声
+            };
+            var result = client.Synthesis(this.synthesis_input.Text, option);
+            if (result.ErrorCode == 0)  // 或 result.Success
+            {
+                File.WriteAllBytes(@"D:\BUAA\SpeechProcessing\samples\合成的语音.mp3", result.Data);
+            }
         }
     }
 }
