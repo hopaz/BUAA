60bac9188a41a9ea5c6d77931564fb02b00cab3c
hoppaz <hoppaz@163.com>
2020-10-20 08:33:01 +0800

    add SpeechProcessing UI

---
+++ SpeechProcessing/SpeechProcessing/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeechProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //选择文件按钮
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.SelectedText = dialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //开始识别按钮
+        }
+    }
+}
