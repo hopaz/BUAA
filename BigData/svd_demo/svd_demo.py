311c26e06c4d9f6ad03be3bbab20fe07b1d92cbd
hoppaz <hoppaz@163.com>
2020-11-01 23:29:37 +0800

    svd_demo completed

--- BigData/svd_demo/svd_demo.py
+++ BigData/svd_demo/svd_demo.py
@@ -3,15 +3,15 @@ from numpy import linalg as la
 import sys
 
 if __name__ == "__main__":
-    # row = np.random.
     set_printoptions(threshold=sys.maxsize)
+    # A是50X200的矩阵，50行代表用户数目，200列代表商家数目
     A = random.randint(0, 10, [50, 200])
-    print(A)
-    print("\n")
     U, S, V = la.svd(A)
+    # U代表50个用户与50个商家合并类的关系？
+    print(U.shape, U)
+    # S表示50个合并类之间的关系？
+    print(S.shape, S)
+    # V表示？？
+    print(V.shape, V)
 
-    # print(U)
-    # print("\n")
-    # print(S)
-    # print("\n")
-    # print(V)
+
