6b3d28ca53a3308b098c4a82dc13c528419ea5cc
zhangjingjing1 <zhangjingjing1@sensetime.com>
2020-12-08 19:59:15 +0800

    basic function of image recognition

---
+++ ProjectPracticeOne/ImageRecognition/ImageRecognition/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ImageRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.cbDetctFace = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnDishes = new System.Windows.Forms.Button();
+            this.btnCar = new System.Windows.Forms.Button();
+            this.btnLogo = new System.Windows.Forms.Button();
+            this.btnAnimal = new System.Windows.Forms.Button();
+            this.btnPlant = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.lbGeneral = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(13, 13);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "打开图像";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // cbDetctFace
+            // 
+            this.cbDetctFace.AutoSize = true;
+            this.cbDetctFace.Location = new System.Drawing.Point(108, 17);
+            this.cbDetctFace.Name = "cbDetctFace";
+            this.cbDetctFace.Size = new System.Drawing.Size(72, 16);
+            this.cbDetctFace.TabIndex = 1;
+            this.cbDetctFace.Text = "检测人脸";
+            this.cbDetctFace.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(11, 342);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(207, 150);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnDishes
+            // 
+            this.btnDishes.Location = new System.Drawing.Point(380, 51);
+            this.btnDishes.Name = "btnDishes";
+            this.btnDishes.Size = new System.Drawing.Size(75, 23);
+            this.btnDishes.TabIndex = 4;
+            this.btnDishes.Text = "菜品识别";
+            this.btnDishes.UseVisualStyleBackColor = true;
+            this.btnDishes.Click += new System.EventHandler(this.btnDishes_Click);
+            // 
+            // btnCar
+            // 
+            this.btnCar.Location = new System.Drawing.Point(474, 51);
+            this.btnCar.Name = "btnCar";
+            this.btnCar.Size = new System.Drawing.Size(75, 23);
+            this.btnCar.TabIndex = 5;
+            this.btnCar.Text = "车型识别";
+            this.btnCar.UseVisualStyleBackColor = true;
+            this.btnCar.Click += new System.EventHandler(this.btnCar_Click);
+            // 
+            // btnLogo
+            // 
+            this.btnLogo.Location = new System.Drawing.Point(568, 51);
+            this.btnLogo.Name = "btnLogo";
+            this.btnLogo.Size = new System.Drawing.Size(75, 23);
+            this.btnLogo.TabIndex = 6;
+            this.btnLogo.Text = "商标识别";
+            this.btnLogo.UseVisualStyleBackColor = true;
+            this.btnLogo.Click += new System.EventHandler(this.btnLogo_Click);
+            // 
+            // btnAnimal
+            // 
+            this.btnAnimal.Location = new System.Drawing.Point(380, 79);
+            this.btnAnimal.Name = "btnAnimal";
+            this.btnAnimal.Size = new System.Drawing.Size(75, 23);
+            this.btnAnimal.TabIndex = 7;
+            this.btnAnimal.Text = "动物识别";
+            this.btnAnimal.UseVisualStyleBackColor = true;
+            this.btnAnimal.Click += new System.EventHandler(this.btnAnimal_Click);
+            // 
+            // btnPlant
+            // 
+            this.btnPlant.Location = new System.Drawing.Point(474, 80);
+            this.btnPlant.Name = "btnPlant";
+            this.btnPlant.Size = new System.Drawing.Size(75, 23);
+            this.btnPlant.TabIndex = 8;
+            this.btnPlant.Text = "植物识别";
+            this.btnPlant.UseVisualStyleBackColor = true;
+            this.btnPlant.Click += new System.EventHandler(this.btnPlant_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(380, 108);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(288, 176);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(236, 342);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(407, 150);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            // 
+            // lbGeneral
+            // 
+            this.lbGeneral.AutoSize = true;
+            this.lbGeneral.Location = new System.Drawing.Point(12, 317);
+            this.lbGeneral.Name = "lbGeneral";
+            this.lbGeneral.Size = new System.Drawing.Size(113, 12);
+            this.lbGeneral.TabIndex = 11;
+            this.lbGeneral.Text = "通用物体及场景识别";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 233);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 504);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbGeneral);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnPlant);
+            this.Controls.Add(this.btnAnimal);
+            this.Controls.Add(this.btnLogo);
+            this.Controls.Add(this.btnCar);
+            this.Controls.Add(this.btnDishes);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.cbDetctFace);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.CheckBox cbDetctFace;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnDishes;
+        private System.Windows.Forms.Button btnCar;
+        private System.Windows.Forms.Button btnLogo;
+        private System.Windows.Forms.Button btnAnimal;
+        private System.Windows.Forms.Button btnPlant;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label lbGeneral;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ProjectPracticeOne/ImageRecognition/ImageRecognition/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImageRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string APPID = "23108740";
+        private string APIKEY = "l2h6aObf67bDPUTzRwKLXQHQ";
+        private string APISECRET = "Mipvg24iSHMewTPpmFYZN6BR41sm0q4h";
+        private string fileName = "";
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.pictureBox1.Load(fileDialog.FileName);
+                fileName = fileDialog.FileName;
+            }
+            //通用物体及场景识别
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(APIKEY, APISECRET);
+            var image = File.ReadAllBytes(fileName);
+            var options = new Dictionary<string, object>{
+	                        {"baike_num", 5}
+	                        };
+            var result = client.AdvancedGeneral(image, options);
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                sb.AppendLine("目标: " + result["result"][i]["keyword"].ToString());
+                sb.AppendLine("置信度: " + result["result"][i]["score"].ToString());
+                sb.AppendLine();
+            }
+            //显示结果
+            this.richTextBox1.Text = sb.ToString();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                bool desExists = result["result"][i]["baike_info"].ToString().Contains("description");
+                bool imageUrlExists = result["result"][i]["baike_info"].ToString().Contains("image_url");
+                if (desExists && imageUrlExists)
+                {
+                    //显示描述
+                    this.richTextBox2.Text = result["result"][i]["baike_info"]["description"].ToString();
+                    //显示图片
+                    try
+                    {
+                        this.pictureBox2.Image = null;
+                        this.pictureBox2.WaitOnLoad = false; //设置为异步加载图片
+                        this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pictureBox2.LoadAsync(result["result"][i]["baike_info"]["image_url"].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("网络链接失败！");
+                    }
+                    break;
+                }
+            }
+
+            //图像主体识别
+            var optionsObject = new Dictionary<string, object>{
+	                        {"with_face", this.cbDetctFace.Checked}
+	                        };
+            var resultObject = client.ObjectDetect(image, options);
+
+            //原图片文件
+            Image fromImage = Image.FromFile(fileName);
+            //创建新图位图
+            Bitmap bitmap = new Bitmap(fromImage.Width, fromImage.Height);
+            //创建作图区域
+            Graphics graphic = Graphics.FromImage(bitmap);
+            //截取原图相应区域写入作图区
+            int x = Convert.ToInt32(resultObject["result"]["left"]);
+            int y = Convert.ToInt32(resultObject["result"]["top"]);
+            int width = Convert.ToInt32(resultObject["result"]["width"]);
+            int height = Convert.ToInt32(resultObject["result"]["height"]);
+            // Create pen.
+            Pen Pen = new Pen(Color.Red, 3);
+            graphic.DrawRectangle(Pen, x, y, width, height);
+            graphic.();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e, JObject resultObject)
+        {
+
+            //原图片文件
+            Image imageOri = Image.FromFile(fileName);
+            Graphics graphic = Graphics.FromImage(imageOri);
+            int x = Convert.ToInt32(resultObject["result"]["left"]);
+            int y = Convert.ToInt32(resultObject["result"]["top"]);
+            int width = Convert.ToInt32(resultObject["result"]["width"]);
+            int height = Convert.ToInt32(resultObject["result"]["height"]);
+            // Create pen.
+            Pen Pen = new Pen(Color.Red, 3);
+            graphic.DrawRectangle(Pen, x, y, width, height);
+
+
+
+        }
+        private void btnDishes_Click(object sender, EventArgs e)
+        {
+            //菜品识别
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(APIKEY, APISECRET);
+            var image = File.ReadAllBytes(fileName);
+            var options = new Dictionary<string, object>{
+	                        {"top_num", 3},
+	                        {"filter_threshold", "0.7"},
+	                        {"baike_num", 5}
+	                        };
+            var result = client.DishDetect(image, options);
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                sb.AppendLine("菜品: " + result["result"][i]["name"].ToString());
+                sb.AppendLine("置信度: " + result["result"][i]["probability"].ToString());
+                sb.AppendLine();
+            }
+            //显示结果
+            this.richTextBox1.Text = sb.ToString();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                bool desExists = result["result"][i]["baike_info"].ToString().Contains("description");
+                bool imageUrlExists = result["result"][i]["baike_info"].ToString().Contains("image_url");
+                if (desExists && imageUrlExists)
+                {
+                    //显示描述
+                    this.richTextBox2.Text = result["result"][i]["baike_info"]["description"].ToString();
+                    //显示图片
+                    try
+                    {
+                        this.pictureBox2.Image = null;
+                        this.pictureBox2.WaitOnLoad = false; //设置为异步加载图片
+                        this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pictureBox2.LoadAsync(result["result"][i]["baike_info"]["image_url"].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("网络链接失败！");
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void btnCar_Click(object sender, EventArgs e)
+        {
+            //车辆识别
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(APIKEY, APISECRET);
+            var image = File.ReadAllBytes(fileName);
+            var options = new Dictionary<string, object>{
+	                        {"top_num", 3},
+                            {"baike_num", 5}
+	                        };
+            var result = client.CarDetect(image, options);
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                sb.AppendLine("车辆: " + result["result"][i]["name"].ToString());
+                sb.AppendLine("置信度: " + result["result"][i]["score"].ToString());
+                sb.AppendLine();
+            }
+            //显示结果
+            this.richTextBox1.Text = sb.ToString();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                bool desExists = result["result"][i]["baike_info"].ToString().Contains("description");
+                bool imageUrlExists = result["result"][i]["baike_info"].ToString().Contains("image_url");
+                if (desExists && imageUrlExists)
+                {
+                    //显示描述
+                    this.richTextBox2.Text = result["result"][i]["baike_info"]["description"].ToString();
+                    //显示图片
+                    try
+                    {
+                        this.pictureBox2.Image = null;
+                        this.pictureBox2.WaitOnLoad = false; //设置为异步加载图片
+                        this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pictureBox2.LoadAsync(result["result"][i]["baike_info"]["image_url"].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("网络链接失败！");
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void btnLogo_Click(object sender, EventArgs e)
+        {
+            //logo识别
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(APIKEY, APISECRET);
+            var image = File.ReadAllBytes(fileName);
+            var result = client.LogoSearch(image);
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                sb.AppendLine("logo: " + result["result"][i]["name"].ToString());
+                sb.AppendLine("置信度: " + result["result"][i]["probability"].ToString());
+                sb.AppendLine();
+            }
+            //显示结果
+            this.richTextBox1.Text = sb.ToString();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                bool desExists = result["result"][i]["baike_info"].ToString().Contains("description");
+                bool imageUrlExists = result["result"][i]["baike_info"].ToString().Contains("image_url");
+                if (desExists && imageUrlExists)
+                {
+                    //显示描述
+                    this.richTextBox2.Text = result["result"][i]["baike_info"]["description"].ToString();
+                    //显示图片
+                    try
+                    {
+                        this.pictureBox2.Image = null;
+                        this.pictureBox2.WaitOnLoad = false; //设置为异步加载图片
+                        this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pictureBox2.LoadAsync(result["result"][i]["baike_info"]["image_url"].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("网络链接失败！");
+                    }
+                    break;
+                }
+            }
+        }
+
+
+        private void btnAnimal_Click(object sender, EventArgs e)
+        {
+            //动物识别
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(APIKEY, APISECRET);
+            var image = File.ReadAllBytes(fileName);
+            var options = new Dictionary<string, object>{
+                            {"top_num", 3},
+	                        {"baike_num", 5}
+	                        };
+            var result = client.AnimalDetect(image, options);
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                sb.AppendLine("动物: " + result["result"][i]["name"].ToString());
+                sb.AppendLine("置信度: " + result["result"][i]["score"].ToString());
+                sb.AppendLine();
+            }
+            //显示结果
+            this.richTextBox1.Text = sb.ToString();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                bool desExists = result["result"][i]["baike_info"].ToString().Contains("description");
+                bool imageUrlExists = result["result"][i]["baike_info"].ToString().Contains("image_url");
+                if (desExists && imageUrlExists)
+                {
+                    //显示描述
+                    this.richTextBox2.Text = result["result"][i]["baike_info"]["description"].ToString();
+                    //显示图片
+                    try
+                    {
+                        this.pictureBox2.Image = null;
+                        this.pictureBox2.WaitOnLoad = false; //设置为异步加载图片
+                        this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pictureBox2.LoadAsync(result["result"][i]["baike_info"]["image_url"].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("网络链接失败！");
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void btnPlant_Click(object sender, EventArgs e)
+        {
+            //植物识别
+            var client = new Baidu.Aip.ImageClassify.ImageClassify(APIKEY, APISECRET);
+            var image = File.ReadAllBytes(fileName);
+            var options = new Dictionary<string, object>{
+	                        {"baike_num", 5}
+	                        };
+            var result = client.PlantDetect(image, options);
+            var sb = new System.Text.StringBuilder();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                sb.AppendLine("植物: " + result["result"][i]["name"].ToString());
+                sb.AppendLine("置信度: " + result["result"][i]["score"].ToString());
+                sb.AppendLine();
+            }
+            //显示结果
+            this.richTextBox1.Text = sb.ToString();
+            for (int i = 0; i < result["result"].Count(); i++)
+            {
+                bool desExists = result["result"][i]["baike_info"].ToString().Contains("description");
+                bool imageUrlExists = result["result"][i]["baike_info"].ToString().Contains("image_url");
+                if (desExists && imageUrlExists)
+                {
+                    //显示描述
+                    this.richTextBox2.Text = result["result"][i]["baike_info"]["description"].ToString();
+                    //显示图片
+                    try
+                    {
+                        this.pictureBox2.Image = null;
+                        this.pictureBox2.WaitOnLoad = false; //设置为异步加载图片
+                        this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pictureBox2.LoadAsync(result["result"][i]["baike_info"]["image_url"].ToString());
+                    }
+                    catch
+                    {
+                        MessageBox.Show("网络链接失败！");
+                    }
+                    break;
+                }
+            }
+        }
+
+    }
+}
